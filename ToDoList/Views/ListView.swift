e942c5f29df3f4d00ba217a873b3f7c7c8570240
Isaad Khan <IKhan@lcs.on.ca>
2023-04-03 14:57:28 -0400

    Re-arrange the project

---
+++ ToDoList/Views/ListView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Isaad Khan on 2023-04-03.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+       
+            Text("Hello, world!")
+        }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
