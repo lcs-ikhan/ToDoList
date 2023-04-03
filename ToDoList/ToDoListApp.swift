1cb6192fa7b59a967ed788330dc7e4fb5105473f
Isaad Khan <IKhan@lcs.on.ca>
2023-04-03 14:49:50 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Isaad Khan on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
